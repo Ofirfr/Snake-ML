570865e678c710e3d0eb5b5dba166b2442a970df
ofirfrisch <ofir.f@youappi.com>
2022-09-26 00:14:22 +0300

    Snake game done

---
+++ snake/Fruits.py
@@ -0,0 +1,41 @@
+from random import Random
+
+
+
+
+FRUIT_COLOR = (0, 255, 0)
+NO_FRUIT_LOCATION = (-1, -1)
+
+rnd = Random()
+class Fruits:
+    
+    fruit_ticker = rnd.randint(5, 15)
+    fruit_exists = False
+    current_fruit_location = NO_FRUIT_LOCATION
+
+    def new_ticker(self):
+        return rnd.randint(5, 15)
+
+    # Updates fruit system, should be ran for every new frame
+    def new_frame(self,snake_body):
+        if (self.fruit_ticker == 0 and not self.fruit_exists):
+            self.new_fruit(snake_body)
+            return
+        self.fruit_ticker -= 1
+
+    def fruit_eaten(self):
+        self.fruit_ticker = self.new_ticker()
+        self.fruit_exists = False
+        self.current_fruit_location = NO_FRUIT_LOCATION
+
+    def new_fruit(self, snake_body):
+        found_fruit_location = False
+        from snake.Snake import BODY_PART_SIZE, HEIGHT, WIDTH
+        while (not found_fruit_location):
+            test_location_x, test_location_y = rnd.randint(
+                0, WIDTH/BODY_PART_SIZE-1)*BODY_PART_SIZE, rnd.randint(0, HEIGHT/BODY_PART_SIZE-1)*BODY_PART_SIZE
+            if (test_location_x, test_location_y) not in snake_body:
+                found_fruit_location = True
+                self.current_fruit_location = (
+                    test_location_x, test_location_y)
+                self.fruit_exists = True
--- snake/Snake.py
+++ snake/Snake.py
@@ -1,19 +1,21 @@
-import Direction
 import pygame as pg
 
+from snake.Fruits import FRUIT_COLOR, NO_FRUIT_LOCATION, Fruits
+from snake.Direction import Direction
 WIDTH, HEIGHT = 512, 512
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
 BODY_COLOR = (255, 0, 0)
-FRUIT_COLOR = (0, 255, 0)
 BACKGROUND_COLOR = (0, 0, 0)
 BODY_PART_SIZE = 16
-FPS = 12
+FPS = 10
 pg.display.set_caption("Snake")
 
 
 class Snake:
     body = []  # body is list of pixels of the body, from tail to head
     direction = Direction.up
+    fruit_eaten = False
+    fruit_system = Fruits()
 
     def __init__(self):
         self.body = [(224, 256), (240, 256), (256, 256)]
@@ -29,20 +31,23 @@ class Snake:
                         self.direction = Direction.go_left(self.direction)
                     if event.key == pg.K_RIGHT:
                         self.direction = Direction.go_right(self.direction)
-
             self.draw_game()
             self.update_body()
             if (self.collisions_found()):
                 run = False
                 print("Collision found!")
+            self.fruit_system.new_frame(self.body)
         pg.quit()
-    
-
-
 
     def update_body(self):
-        del self.body[0]
+        if (self.fruit_eaten):  # Fruit eaten, dont delete tail
+            self.fruit_eaten = False
+        else:
+            del self.body[0]
         new_part_x, new_part_y = self.get_new_part()
+        if (new_part_x, new_part_y) == self.fruit_system.current_fruit_location:
+            self.fruit_eaten = True
+            self.fruit_system.fruit_eaten()
         self.body.append((new_part_x, new_part_y))
 
     def get_new_part(self):
@@ -59,9 +64,9 @@ class Snake:
     def collisions_found(self):
         head = self.body[-1]
         # Check if body part got out of bounds
-        if (head[0]>WIDTH-BODY_PART_SIZE or head[0]<0 or head[1]>HEIGHT-BODY_PART_SIZE or head[1]<0):
+        if (head[0] > WIDTH-BODY_PART_SIZE or head[0] < 0 or head[1] > HEIGHT-BODY_PART_SIZE or head[1] < 0):
             print("Out of bounds!")
-            print("Head was at ",head)
+            print("Head was at ", head)
             return True
         # Check if body part got over another body part
         counted_parts = set()
@@ -72,11 +77,14 @@ class Snake:
             counted_parts.add(part)
         return False
 
-
-
     def draw_game(self):
         WIN.fill(BACKGROUND_COLOR)
         pixels = pg.PixelArray(WIN)
+        if (self.fruit_system.fruit_exists):
+            for x in range(1, BODY_PART_SIZE-1):
+                for y in range(1, BODY_PART_SIZE-1):
+                    pixels[self.fruit_system.current_fruit_location[0]+x,
+                           self.fruit_system.current_fruit_location[1]+y] = FRUIT_COLOR
         for body_part in self.body:
             for x in range(1, BODY_PART_SIZE-1):
                 for y in range(1, BODY_PART_SIZE-1):
